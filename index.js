4a63bf872ec81ce2757d65e97a8c9f1c09edb59c
DevRowdra <121636096+DevRowdra@users.noreply.github.com>
2023-05-02 10:57:25 +0600

    add chef json data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const chef=require('./Chef.json')
 cors = require('cors')
 const port = 3000
 app.use(cors())
@@ -7,6 +8,9 @@ app.get('/', (req, res) => {
     res.send('my 10th assignment  10')
   })
   
+  app.get('/chef',(req,res)=>{
+    res.send(chef)
+  })
   app.listen(port, () => {
     console.log(` port on ${port}`)
   })
